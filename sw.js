b41f9466a669fdab3a315b87115b1905213d0c53
tjjngs <sqqqhm@163.com>
2024-12-11 12:16:03 +0800

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'calculator-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  './index.html',
+  './style.css',
+  './script.js',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
